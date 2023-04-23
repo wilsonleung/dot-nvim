5ce821f7eb4f1fee355ed48ae8892456c03fec51
Wilson Leung <wilsonpsleung@gmail.com>
2023-04-24 07:59:11 +0800

    add neovide settings

--- init.lua
+++ init.lua
@@ -1,3 +1,9 @@
 require("user.opts")
 require("user.maps")
 require("user.plugins")
+
+-- neovide settings
+if vim.g.neovide then
+	vim.g.neovide_transparency = 0.9
+	vim.g.neovide_cursor_animation_length = 0
+end
