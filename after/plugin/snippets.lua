9d307559418d4c99c59a3e0061c8e8035127a951
Wilson Leung <wilsonpsleung@gmail.com>
2023-04-30 18:59:44 +0800

    add custom snippets

---
+++ after/plugin/snippets.lua
@@ -0,0 +1,39 @@
+local status, ls = pcall(require, "luasnip")
+if not status then
+	return
+end
+
+-- some shorthands...
+local snip = ls.snippet
+local node = ls.snippet_node
+local text = ls.text_node
+local insert = ls.insert_node
+local func = ls.function_node
+local choice = ls.choice_node
+local dynamicn = ls.dynamic_node
+
+local date = function()
+	return { os.date("%Y-%m-%d") }
+end
+
+local function captialized(str)
+	return str:gsub("^%l", string.upper)
+end
+
+ls.add_snippets(nil, {
+	typescriptreact = {
+		snip({
+			trig = "useState",
+			namr = "useState",
+			dscr = "react useState hook",
+		}, {
+			text("const ["),
+			insert(1, "state"),
+			text(", set"),
+			func(function(arg, _)
+				return captialized(arg[1][1])
+			end, { 1 }),
+			text("] = useState()"),
+		}),
+	},
+})
