f2b156378d30dcb259e91c37ce07842d353018d0
Wilson Leung <wilsonpsleung@gmail.com>
2023-05-01 17:03:53 +0800

    update null-ls config

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -4,27 +4,53 @@ if not setup then
 	return
 end
 
--- for conciseness
-local formatting = null_ls.builtins.formatting -- to setup formatters
-local diagnostics = null_ls.builtins.diagnostics -- to setup linters
-
 -- to setup format on save
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- set style/lint files
+local eslint_root_files = { ".eslintrc", ".eslintrc.js", ".eslintrc.json" }
+local prettier_root_files = { ".prettierrc", ".prettierrc.js", ".prettierrc.json" }
+local stylua_root_files = { "stylua.toml", ".stylua.toml" }
+local elm_root_files = { "elm.json" }
+
+local root_has_file = function(files)
+	return function(utils)
+		return utils.root_has_file(files)
+	end
+end
+
+local opts = {
+	eslint_formatting = {
+		condition = function(utils)
+			local has_eslint = root_has_file(eslint_root_files)(utils)
+			local has_prettier = root_has_file(prettier_root_files)(utils)
+			return has_eslint and not has_prettier
+		end,
+	},
+	eslint_diagnostics = {
+		condition = root_has_file(eslint_root_files),
+	},
+	prettier_formatting = {
+		condition = root_has_file(prettier_root_files),
+	},
+	stylua_formatting = {
+		condition = root_has_file(stylua_root_files),
+	},
+	elm_format_formatting = {
+		condition = root_has_file(elm_root_files),
+	},
+}
 
 -- configure null_ls
 null_ls.setup({
 	-- setup formatters & linters
 	sources = {
-		--  to disable file types use
-		--  "formatting.prettier.with({disabled_filetypes = {}})" (see null-ls docs)
-		formatting.prettier, -- js/ts formatter
-		formatting.stylua, -- lua formatter
-		diagnostics.eslint_d.with({ -- js/ts linter
-			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-			condition = function(utils)
-				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-			end,
-		}),
+		null_ls.builtins.diagnostics.eslint_d.with(opts.eslint_diagnostics),
+		null_ls.builtins.formatting.eslint_d.with(opts.eslint_formatting),
+		null_ls.builtins.formatting.prettier.with(opts.prettier_formatting),
+		null_ls.builtins.formatting.stylua.with(opts.stylua_formatting),
+		null_ls.builtins.formatting.elm_format.with(opts.elm_format_formatting),
+		null_ls.builtins.code_actions.eslint_d.with(opts.eslint_diagnostics),
 	},
 	-- configure format on save
 	on_attach = function(current_client, bufnr)
