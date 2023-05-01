cbeb5693dcbf63bd13e556329caf2b5dd0aca41d
Wilson Leung <wilsonpsleung@gmail.com>
2023-05-01 22:15:44 +0800

    add toggle term support

---
+++ after/plugin/toggleterm.lua
@@ -0,0 +1,52 @@
+local setup, term = pcall(require, "toggleterm")
+if not setup then
+	return
+end
+
+term.setup {
+  size = 15,
+  open_mapping = [[<C-\>]],
+  hide_numbers = true,
+  shade_filetypes = {},
+  shade_terminals = true,
+  shading_factor = 2,
+  start_in_insert = true,
+  insert_mappings = true,
+  persist_size = true,
+  direction = "horizontal",
+  close_on_exit = true,
+  shell = vim.o.shell,
+  float_opts = {
+    border = "curved",
+    winblend = 0,
+    highlights = {
+      border = "Normal",
+      background = "Normal",
+    },
+  },
+}
+
+local Terminal = require("toggleterm.terminal").Terminal
+local lazygit = Terminal:new {
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "single",
+  },
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+    -- vim.cmd "set laststatus=0"
+  end,
+  on_close = function(_)
+    -- vim.cmd "set laststatus=3"
+  end,
+  count = 99,
+}
+
+function _LAZYGIT_TOGGLE()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _LAZYGIT_TOGGLE()<CR>", {noremap = true, silent = true})
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,109 +1,112 @@
 local ensure_packer = function()
-	local fn = vim.fn
-	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-	if fn.empty(fn.glob(install_path)) > 0 then
-		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-		vim.cmd([[packadd packer.nvim]])
-		return true
-	end
-	return false
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
-	use("wbthomason/packer.nvim")
+  use("wbthomason/packer.nvim")
 
-	-- ======================
-	-- users' plugin
-	-- ======================
+  -- ======================
+  -- users' plugin
+  -- ======================
 
-	-- color scheme
-	use("sainnhe/gruvbox-material")
+  -- color scheme
+  use("sainnhe/gruvbox-material")
 
-	-- show indent vertical line
-	use("lukas-reineke/indent-blankline.nvim")
+  -- show indent vertical line
+  use("lukas-reineke/indent-blankline.nvim")
 
-	-- Common utilities
-	use({ "nvim-lua/plenary.nvim", tag = "v0.1.3" })
+  -- Common utilities
+  use({ "nvim-lua/plenary.nvim", tag = "v0.1.3" })
 
-	-- Statusline
-	use({ "nvim-lualine/lualine.nvim", tag = "compat-nvim-0.6" })
+  -- Statusline
+  use({ "nvim-lualine/lualine.nvim", tag = "compat-nvim-0.6" })
 
-	-- commenting with gc
-	use({
-		"numToStr/Comment.nvim",
-		tag = "v0.8.0",
-		requires = {
-			"JoosepAlviste/nvim-ts-context-commentstring",
-		},
-	})
+  -- toggle terminal
+  use { "akinsho/toggleterm.nvim", tag = '*' }
 
-	-- file explorer
-	use("nvim-tree/nvim-tree.lua")
+  -- commenting with gc
+  use({
+    "numToStr/Comment.nvim",
+    tag = "v0.8.0",
+    requires = {
+      "JoosepAlviste/nvim-ts-context-commentstring",
+    },
+  })
 
-	-- fuzzy finding w/ telescope
-	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
+  -- file explorer
+  use("nvim-tree/nvim-tree.lua")
 
-	-- vs-code like icons
-	use("nvim-tree/nvim-web-devicons")
+  -- fuzzy finding w/ telescope
+  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" })
 
-	-- show line modifications on left hand side
-	use({ "lewis6991/gitsigns.nvim", tag = "v0.6" })
+  -- vs-code like icons
+  use("nvim-tree/nvim-web-devicons")
 
-	-- tmux & split window navigation
-	use("christoomey/vim-tmux-navigator")
+  -- show line modifications on left hand side
+  use({ "lewis6991/gitsigns.nvim", tag = "v0.6" })
 
-	-- snippets
-	use({ "L3MON4D3/LuaSnip", tag = "v1.2.1", run = "make install_jsregexp" }) -- snippet engine
-	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-	use("rafamadriz/friendly-snippets") -- useful snippets
+  -- tmux & split window navigation
+  use("christoomey/vim-tmux-navigator")
 
-	-- autocompletion
-	use("onsails/lspkind-nvim") -- vscode-like pictograms
-	use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words
-	use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim's built-in LSP
-	use({ "hrsh7th/nvim-cmp", tag = "v0.0.1" }) -- Completion
+  -- snippets
+  use({ "L3MON4D3/LuaSnip", tag = "v1.2.1", run = "make install_jsregexp" }) -- snippet engine
+  use("saadparwaiz1/cmp_luasnip")                                            -- for autocompletion
+  use("rafamadriz/friendly-snippets")                                        -- useful snippets
 
-	-- managing & installing lsp servers, linters & formatters
-	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
-	use("ray-x/lsp_signature.nvim")
+  -- autocompletion
+  use("onsails/lspkind-nvim")                 -- vscode-like pictograms
+  use("hrsh7th/cmp-buffer")                   -- nvim-cmp source for buffer words
+  use("hrsh7th/cmp-nvim-lsp")                 -- nvim-cmp source for neovim's built-in LSP
+  use({ "hrsh7th/nvim-cmp", tag = "v0.0.1" }) -- Completion
 
-	-- configuring lsp servers
-	use("neovim/nvim-lspconfig") -- easily configure language servers
-	use({
-		"glepnir/lspsaga.nvim",
-		tag = "v0.2.9",
-		requires = {
-			{ "nvim-tree/nvim-web-devicons" },
-			{ "nvim-treesitter/nvim-treesitter" },
-		},
-	}) -- enhanced lsp uis
-	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+  -- managing & installing lsp servers, linters & formatters
+  use("williamboman/mason.nvim")           -- in charge of managing lsp servers, linters & formatters
+  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+  use("ray-x/lsp_signature.nvim")
 
-	-- formatting & linting
-	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+  -- configuring lsp servers
+  use("neovim/nvim-lspconfig") -- easily configure language servers
+  use({
+    "glepnir/lspsaga.nvim",
+    tag = "v0.2.9",
+    requires = {
+      { "nvim-tree/nvim-web-devicons" },
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+  })                                        -- enhanced lsp uis
+  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
 
-	-- treesitter configuration
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = function()
-			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-			ts_update()
-		end,
-	})
+  -- formatting & linting
+  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+  use("jayp0521/mason-null-ls.nvim")     -- bridges gap b/w mason & null-ls
 
-	-- auto closing
-	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+  -- treesitter configuration
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = function()
+      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+      ts_update()
+    end,
+  })
 
-	-- ======================
-	-- Automatically set up your configuration after cloning packer.nvim
-	-- Put this at the end after all plugins
-	-- ======================
-	if packer_bootstrap then
-		require("packer").sync()
-	end
+  -- auto closing
+  use("windwp/nvim-autopairs")                                 -- autoclose parens, brackets, quotes, etc...
+  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+
+  -- ======================
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  -- ======================
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
