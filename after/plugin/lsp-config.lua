4b42ec4a38e2509b40ccaacf3f1364d696d446c4
Wilson Leung <wilsonpsleung@gmail.com>
2023-05-02 22:35:58 +0800

    update react related shortcut

--- after/plugin/lsp-config.lua
+++ after/plugin/lsp-config.lua
@@ -55,8 +55,9 @@ local on_attach = function(client, bufnr)
 
 	-- typescript specific keymaps (e.g. rename file and update imports)
 	if client.name == "tsserver" then
-		keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
+    keymap.set("n", "<leader>mi", ":TypescriptAddMissingImports<CR>") -- add missing import 
 		keymap.set("n", "<leader>oi", ":TypescriptOrganizeImports<CR>") -- organize imports (not in youtube nvim video)
+    keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>") -- rename file and update imports
 		keymap.set("n", "<leader>ru", ":TypescriptRemoveUnused<CR>") -- remove unused variables (not in youtube nvim video)
 	end
 
