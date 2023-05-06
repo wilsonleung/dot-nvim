68d3714f34d1396847710fe1384582296ca45c1a
Wilson Leung <wilsonpsleung@gmail.com>
2023-05-06 14:02:12 +0800

    update lspsaga border

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -1,22 +1,25 @@
 -- import lspsaga safely
 local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-	return
+  return
 end
 
 saga.setup({
-	ui = {
-		title = true,
-		winblend = 10,
-		border = "rounded",
-		colors = {
-			normal_bg = "#282828",
-		},
-	},
-	-- keybinds for navigation in lspsaga window
-	scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
-	-- use enter to open file with definition preview
-	definition = {
-		edit = "<CR>",
-	},
+  ui = {
+    title = true,
+    winblend = 0,
+    border = "single",
+  },
+  symbol_in_winbar = {
+    enable = false
+  },
+  -- keybinds for navigation in lspsaga window
+  scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
+  -- use enter to open file with definition preview
+  definition = {
+    edit = "<CR>",
+  },
+  diagnostic = {
+    border_follow =  false,
+  },
 })
