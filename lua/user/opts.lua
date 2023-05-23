d5b62a9ca645d582a22b3ee86d892def9f2ad89d
Wilson Leung <wilsonpsleung@gmail.com>
2023-05-23 17:56:38 +0800

    disable wsl paste fisrt due to unexpected error

--- lua/user/opts.lua
+++ lua/user/opts.lua
@@ -82,14 +82,14 @@ if is_wsl then
       ["+"] = { "clip.exe" },
       ["*"] = { "clip.exe" }
     },
-    paste = {
-      ["+"] = {
-        'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
-      },
-      ["*"] = {
-        'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
-      },
-    },
+    -- paste = {
+    --   ["+"] = {
+    --     'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+    --   },
+    --   ["*"] = {
+    --     'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))'
+    --   },
+    -- },
     cache_enabled = true
   }
 end
