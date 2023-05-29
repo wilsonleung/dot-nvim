3f7062813211859e75f780a392edaefbee5082a5
Wilson Leung <wilsonpsleung@gmail.com>
2023-05-29 16:13:32 +0800

    disable italic on keywords

--- after/plugin/gruvbox.lua
+++ after/plugin/gruvbox.lua
@@ -2,10 +2,12 @@
 -- vim.g.gruvbox_material_background = "hard"
 -- vim.g.gruvbox_material_better_performance = 1
 -- vim.cmd("colorscheme gruvbox-material")
-vim.g.gruvbox_flat_style = "dark"
-vim.g.gruvbox_flat_style = "dark"
-vim.g.gruvbox_italic_comments = false
 -- vim.g.gruvbox_flat_background = "hard"
 -- vim.g.gruvbox_flat_better_performance = 1
+--
+vim.g.gruvbox_flat_style = "dark"
+
+vim.g.gruvbox_italic_comments = false
+vim.g.gruvbox_italic_keywords = false
 
 vim.cmd("colorscheme gruvbox-flat")
