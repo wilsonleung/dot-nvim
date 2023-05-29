5074bf73a3eeb47b495db08ef288b07037697db3
Wilson Leung <wilsonpsleung@gmail.com>
2023-05-29 15:54:46 +0800

    disable comment in italic

--- after/plugin/gruvbox.lua
+++ after/plugin/gruvbox.lua
@@ -3,6 +3,8 @@
 -- vim.g.gruvbox_material_better_performance = 1
 -- vim.cmd("colorscheme gruvbox-material")
 vim.g.gruvbox_flat_style = "dark"
+vim.g.gruvbox_flat_style = "dark"
+vim.g.gruvbox_italic_comments = false
 -- vim.g.gruvbox_flat_background = "hard"
 -- vim.g.gruvbox_flat_better_performance = 1
 
